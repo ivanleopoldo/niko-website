c63d9c23be63497426c8982c2c428cc618c4f1e1
ivaintwc <75525118+ivanleopoldo@users.noreply.github.com>
2023-05-04 12:50:48 +0800

    added tailwindcss, changed file structure
    added tailwindcss
    changed routing
    changed file structure

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <p class="font-bold text-3xl">About Page</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import logo from '../resources/logo.svg';
+import '../css/Home.css';
+
+function Home() {
+  return (
+    <div className="body">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
