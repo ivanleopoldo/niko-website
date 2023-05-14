c5d979375e39e3adbffcad4aa1201c9428462621
Joel T. Jayme <67952167+Noob11413@users.noreply.github.com>
2023-05-15 02:48:05 +0800

    Message modals and Freelancer UI

--- src/components/Fcard.js
+++ src/components/Fcard.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Messages from './Messages';
 
-function Card({image, name, bio, date, tags}) {
+function Fcard({image, name, bio, date, tags}) {
   return (
     <div className='card' >
+        <p>User Pr</p>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
           <img src={image} alt="User Image" className="rounded-xl" />
@@ -25,9 +27,9 @@ function Card({image, name, bio, date, tags}) {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
            </svg>
-            <button className="btn btn-primary ml-24 mt-10">Hire now!</button>
-            
-            <div>
+           <div class="flex justify-center">
+                <button class="btn btn-success">Accept</button>  
+                <button class="btn btn-error ml-2.5">Decline</button>
             </div>
           </div>
         </div>
@@ -36,4 +38,4 @@ function Card({image, name, bio, date, tags}) {
   );
 }
 
-export default Card;
+export default Fcard;
--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,8 @@ import Footer from "./Footer";
 import Modal from "./Modal";
 import Navbar from "./Navbar";
 import Pagination from "./Pagination";
-
-export { Card, Content, DrawerLeft, Footer, Modal, Navbar, Pagination };
+import Freelancer_Rqt from "./Freelancer_Rqt";
+import MessageModal from "./MessageModal";
+import Message from "./Messages";
+import Fcard from "./Fcard";
+export { Card, Content, DrawerLeft, Footer, Modal, Navbar, Pagination, Freelancer_Rqt, MessageModal,Message,Fcard};
