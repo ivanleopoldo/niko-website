c6e3fa9f28001576d6d3088af5d3b1ba486eb3a0
Joel T. Jayme <threadjayme@gmail.com>
2023-10-24 23:28:35 +0800

    Updated messages version 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -58,7 +58,7 @@ function Navbar() {
                 <div className="dropdown dropdown-end">
                 <div style={{ position: 'relative', alignItems: 'flex-end' }}>
                         <label tabIndex="0" className="btn btn-ghost">
-                        <span className="badge badge-xs badge-error indicator-item">4</span>
+                        <span className="badge badge-xs badge-error indicator-item"></span>
                          
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                 <path d="M5.85 3.5a.75.75 0 00-1.117-1 9.719 9.719 0 00-2.348 4.876.75.75 0 001.479.248A8.219 8.219 0 015.85 3.5zM19.267 2.5a.75.75 0 10-1.118 1 8.22 8.22 0 011.987 4.124.75.75 0 001.48-.248A9.72 9.72 0 0019.266 2.5z" />
@@ -85,8 +85,11 @@ function Navbar() {
                         <path fillRule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z" clipRule="evenodd" />
                     </svg>
                 </button>
+                <ul class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-42">
 
-                {isMessageCardVisible && <MessageCard style={{ maxHeight: '300px', overflowY: '300px' }} />}
+                      {isMessageCardVisible && <MessageCard style={{ maxHeight: '300px', overflowY: '300px' }} />}
+                </ul>
+              
             </div>
                 <div class="flex-none gap-2">
                     <div class="form-control"></div>
